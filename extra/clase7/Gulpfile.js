690f91045918da523da44521d40e596c8aea2514
Ali Rivera <ali.rivera.echegaray@gmail.com>
2020-04-24 14:06:06 -0300

    clase npm

---
+++ extra/clase7/Gulpfile.js
@@ -0,0 +1,41 @@
+// require me trae un paquete que me instale o un archivo javascript
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+
+gulp.task('sass', function crearSass() {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch:solocss', function solocss() {
+  gulp.watch('./sass/*.scss', gulp.series('sass'));  // If a file changes, re-run 'sass'
+});
+
+
+// Manera 1
+
+gulp.task('minify', function minificar() {
+  return gulp.src('./css/styles.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('./css'));
+})
+
+gulp.task('watch:cssMasMin', function cssMasMin() {
+  gulp.watch('./sass/*.scss', gulp.series(['sass', 'minify']));
+})
+
+//Manera 2
+
+function minificar2(){
+  return gulp.src('./css/styles.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('./css'));
+}
+
+gulp.task('minify2', gulp.series(['sass' , minificar2]));
+
+gulp.task('watch:cssMasMinV2', function cssMasMinV2(){
+  gulp.watch('./sass/*.scss', gulp.series('minify2'));
+})
---
+++ extra/clase7/index.js
@@ -0,0 +1,14 @@
+console.log('hola');
+console.log('hola Wendy hermosa');
+
+function cambios(){
+  console.log("todo cambia");
+}
+
+cambios();
+
+const otroCambio = () => {
+  console.log('otro cambio');
+}
+
+otroCambio();
